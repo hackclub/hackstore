ecd7a6935845867e4fd4bf6bf79ae056fefd7198
bajpai244 <bajpaiharsh244@gmail.com>
2020-08-07 16:41:27 +0530

    added favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,28 @@
-import * as React from "react";
-import NextApp from "next/app";
-import Head from "next/head";
+import * as React from 'react'
+import NextApp from 'next/app'
+import Head from 'next/head'
 
-import Meta from "@hackclub/meta";
-import "@hackclub/theme/fonts/reg-bold.css";
-import theme from "../Theme/theme";
-import { ThemeProvider } from "theme-ui";
-import "simplebar/dist/simplebar.min.css";
-import "../index.css";
+//import Meta from '@hackclub/meta'
+import Meta from '../Components/meta/index'
+import '@hackclub/theme/fonts/reg-bold.css'
+import theme from '../Theme/theme'
+import { ThemeProvider } from 'theme-ui'
+import '../index.css'
 /**index.css contains normalize.css for consistency across multiple browser and also has some global css (in the end of file) */
 
-import { Provider } from "react-redux";
-import store from "../State/store/index";
+import { Provider } from 'react-redux'
+import store from '../State/store/index'
 
 export default class App extends NextApp {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
         <Provider store={store}>
-          <Meta as={Head} />
+          <Meta />
           <Component {...pageProps} />
         </Provider>
       </ThemeProvider>
-    );
+    )
   }
 }
