a85300b0f8ae7e1304a360f40f9a1d97c384de15
bajpai244 <bajpaiharsh244@gmail.com>
2020-11-16 21:09:03 +0530

    search feature added

--- Components/appwindow/index.js
+++ Components/appwindow/index.js
@@ -1,92 +1,117 @@
 /**@jsx jsx*/
 
-import { jsx, Grid } from "theme-ui";
-import { connect } from "react-redux";
-import Renderer from "./renderer";
-import Workshops from "./workshops";
+import { jsx, Grid, Box, Flex } from 'theme-ui'
+import { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import Renderer from './renderer'
+import Workshops from './workshops'
 
-import store from "../../State/store/index";
-import { activetabchanged } from "../../State/actions/index";
+import store from '../../State/store/index'
+import { activetabchanged } from '../../State/actions/index'
 
-import { carddata } from "../sidebar/index";
+import search from '../../lib/search/index'
 
-import mapper from "../../lib/maps/mapper";
+import { carddata } from '../sidebar/index'
 
-const AppWindow = ({ activetab }) => (
-  <div
-    className="appwindow"
-    sx={{
-      flex: 1,
-      bg: "containercolor",
-      height: "100vh",
-      overflow: "auto",
-      display: activetab === "Workshops" ? "flex" : "",
-      flexDirection: activetab === "Workshops" ? "column" : "",
-    }}
-  >
+import mapper from '../../lib/maps/mapper'
+
+import SearchBar from 'material-ui-search-bar'
+
+const AppWindow = ({ activetab }) => {
+  const [data, setdata] = useState(mapper(activetab))
+
+  useEffect(() => {
+    setdata(mapper(activetab))
+    return () => null
+  }, [activetab])
+
+  return (
     <div
-      className="appwindowHead"
+      className='appwindow'
       sx={{
-        pt: 4,
-        width: "100%",
-        justifyContent: "center",
-        background: `linear-gradient(107.91deg, #EC3750 0.04%, #AE0018 97.9%)`,
-        color: "background",
-        pb: 5,
-        alignItems: "center",
-      }}
-    >
-      <div sx={{ color: "white", width: "100%" }}>
-        <div
+        flex: 1,
+        bg: 'containercolor',
+        height: '100vh',
+        overflow: 'auto',
+        display: activetab === 'Workshops' ? 'flex' : '',
+        flexDirection: activetab === 'Workshops' ? 'column' : '',
+      }}>
+      <div
+        className='appwindowHead'
+        sx={{
+          pt: 4,
+          width: '100%',
+          justifyContent: 'center',
+          background: `linear-gradient(107.91deg, #EC3750 0.04%, #AE0018 97.9%)`,
+          color: 'background',
+          pb: 5,
+          alignItems: 'center',
+        }}>
+        <Flex
           sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            flexWrap: "wrap",
-          }}
-        >
-          <img
-            sx={{ width: ["100px", "120px"], justifySelf: "end" }}
-            src={mapper(activetab).imgsrc}
+            position: 'absolute',
+            zIndex: 3,
+            right: [2, 3, 4, 5],
+            top: [2, 3],
+          }}>
+          <SearchBar
+            placeholder='search hackstore'
+            onChange={(newValue) => search(newValue, setdata, activetab)}
+            onRequestSearch={() => null}
           />
-          <h1 sx={{ ml: [2, 4], fontSize: [5, 6] }}>{activetab}</h1>
-        </div>
-        <div sx={{ width: "100%", textAlign: "center" }}>
-          <h1 sx={{ mt: 0 }}>{mapper(activetab).text}</h1>
+        </Flex>
+        <div sx={{ color: 'white', width: '100%' }}>
+          <div
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexWrap: 'wrap',
+            }}>
+            <img
+              sx={{ width: ['100px', '120px'], justifySelf: 'end' }}
+              src={mapper(activetab).imgsrc}
+            />
+            <h1 sx={{ ml: [2, 4], fontSize: [5, 6] }}>{activetab}</h1>
+          </div>
+          <div sx={{ width: '100%', textAlign: 'center' }}>
+            <h1 sx={{ mt: 0 }}>{mapper(activetab).text}</h1>
+          </div>
         </div>
       </div>
-    </div>
 
-    <Grid
-      columns={[carddata.length]}
-      className="mobileSelector"
-      sx={{
-        overflow: "scroll",
-        //    scrollbarWidth: 'none',
-        pt: 5,
-        display: [null, "none"],
-      }}
-    >
-      {carddata.map((ele) => (
-        <img
-          sx={{ width: "100px" }}
-          key={ele.imgsrc}
-          src={ele.imgsrc}
-          onClick={() => store.dispatch(activetabchanged(ele.section))}
-        />
-      ))}
-    </Grid>
-    <br sx={{ display: [null, "none"] }} />
-    <div
-      sx={{
-        pt: activetab === "Workshops" ? [0] : [5],
-        flex: activetab === "Workshops" ? 1 : "",
-      }}
-    >
-      {activetab === "Workshops" ? <Workshops /> : <Renderer />}
+      <Grid
+        columns={[carddata.length]}
+        className='mobileSelector'
+        sx={{
+          overflow: 'scroll',
+          pt: 5,
+          display: [null, 'none'],
+        }}>
+        {carddata.map((ele) => (
+          <img
+            sx={{ width: '100px' }}
+            key={ele.imgsrc}
+            src={ele.imgsrc}
+            onClick={() => store.dispatch(activetabchanged(ele.section))}
+          />
+        ))}
+      </Grid>
+      <br sx={{ display: [null, 'none'] }} />
+      <div
+        sx={{
+          pt: activetab === 'Workshops' ? [0] : [5],
+          flex: activetab === 'Workshops' ? 1 : '',
+        }}>
+        {activetab === 'Workshops' ? (
+          <Workshops />
+        ) : (
+          <Renderer data={data} activetab={activetab} />
+        )}
+      </div>
     </div>
-  </div>
-);
+  )
+}
 
 export const AppCard = ({ imgsrc, appname, rating, target }) => (
   <div
@@ -94,62 +119,61 @@ export const AppCard = ({ imgsrc, appname, rating, target }) => (
       //       border: '1px solid',
       pt: 4,
       pb: [4, 2],
-      width: ["300px", "200px"],
-      bg: "white",
+      width: ['300px', '200px'],
+      bg: 'white',
       borderRadius: 10,
       boxShadow: `0px 4px 30px rgba(0, 0, 0, 0.25)`,
-      ":hover": {
-        transform: "scale(1.1)",
-        cursor: "pointer",
+      ':hover': {
+        transform: 'scale(1.1)',
+        cursor: 'pointer',
       },
     }}
     onClick={() => {
-      console.log(target);
+      console.log(target)
       window.open(
         target
           ? `./app/${target}`
-          : `./app/${appname == "C++" ? "Cplus" : appname.split(" ")[0]}`,
-        "_blank"
-      );
+          : `./app/${appname == 'C++' ? 'Cplus' : appname.split(' ')[0]}`,
+        '_blank'
+      )
     }}
     /**Note: Name of C++ had some issues hence used the above mapping of Cplus */
   >
-    <div sx={{ width: "100%", textAlign: "center" }}>
-      <img sx={{ width: "80px" }} src={imgsrc} />
+    <div sx={{ width: '100%', textAlign: 'center' }}>
+      <img sx={{ width: '80px' }} src={imgsrc} />
     </div>
     <h2
       sx={{
         m: 0,
-        mx: "auto",
-        width: "80%",
-        overflow: "hidden",
-        whiteSpace: "nowrap",
-        textOverflow: "ellipsis",
-        maxWidth: "100%",
-        textAlign: "center",
-        color: "",
-      }}
-    >
+        mx: 'auto',
+        width: '80%',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        maxWidth: '100%',
+        textAlign: 'center',
+        color: '',
+      }}>
       {appname}
     </h2>
-    <div sx={{ width: "100%", textAlign: "center", mt: 2 }}>
+    <div sx={{ width: '100%', textAlign: 'center', mt: 2 }}>
       {looparray.map((ele) =>
         ele <= rating ? (
-          <img sx={{ width: "20px" }} src="starfilled.png" />
+          <img sx={{ width: '20px' }} src='starfilled.png' />
         ) : (
-          <img sx={{ width: "20px" }} src="starblank.png" />
+          <img sx={{ width: '20px' }} src='starblank.png' />
         )
       )}
     </div>
   </div>
-);
+)
 
 export default connect(
   (state) => ({
     activetab: state.activetab,
   }),
   null
-)(AppWindow);
+)(AppWindow)
 
 //used for star rating system
-const looparray = [1, 2, 3, 4, 5];
+const looparray = [1, 2, 3, 4, 5]
--- Components/appwindow/renderer.js
+++ Components/appwindow/renderer.js
@@ -1,37 +1,28 @@
 /**@jsx jsx*/
-import { jsx, Grid } from "theme-ui";
-import { connect } from "react-redux";
-import { AppCard } from "./index";
-import mapper from "../../lib/maps/mapper";
-const Renderer = ({ activetab }) => {
-  const keyinitial = mapper(activetab).keyinitial;
+import { jsx, Grid } from 'theme-ui'
+import { AppCard } from './index'
+
+export default ({ data, activetab }) => {
+  const keyinitial = activetab
 
   return (
     <Grid
       columns={[1, 1, 2, 4, 5]}
       sx={{
-        justifyContent: "center",
-        justifyItems: "center",
+        justifyContent: 'center',
+        justifyItems: 'center',
         rowGap: 5,
         pb: 4,
-      }}
-    >
-      {mapper(activetab).data.map((ele, index) => (
+      }}>
+      {data.data.map((ele, index) => (
         <AppCard
           imgsrc={ele.imgsrc}
           appname={ele.appname}
           rating={ele.ratings}
           target={ele.target}
-          key={`${keyinitial}${index}`}
+          key={`${keyinitial}${ele.appname}`}
         />
       ))}
     </Grid>
-  );
-};
-
-export default connect(
-  (state) => ({
-    activetab: state.activetab,
-  }),
-  null
-)(Renderer);
+  )
+}
